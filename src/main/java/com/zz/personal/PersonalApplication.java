b5689af70131153c411c07acd73d45972d8ec5b4
lizhi <lizhia@zbj.com>
2018-02-27 15:33:27 +0800

    项目打包,正常运行jar包

--- src/main/java/com/zz/personal/PersonalApplication.java
+++ src/main/java/com/zz/personal/PersonalApplication.java
@@ -1,16 +1,18 @@
 package com.zz.personal;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@SpringBootApplication
+//@MapperScan("com.zz.personal.dao.mapper")
+//@SpringBootApplication
 @SpringBootApplication
-@MapperScan("com.zz.personal.dao.mapper")
 public class PersonalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalApplication.class, args);
 	}
+
 }
